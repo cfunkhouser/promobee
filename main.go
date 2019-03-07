1a03fed6a311eb14cbbf92caf12d50bce74c6867
Christian Funkhouser <christian.funkhouser@gmail.com>
2019-03-06 20:39:52 -0500

    Per-thermostat exporter

--- main.go
+++ main.go
@@ -7,92 +7,48 @@ import (
 	"time"
 
 	"github.com/cfunkhouser/egobee"
-	"github.com/prometheus/client_golang/prometheus"
+	"github.com/cfunkhouser/promobee/promobee"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-// The Ecobee App ID Key for Promobee.
-const appID = "94fl7gSO9SFmTXBKr0aSjzMwkjXAIRnZ"
-
 var (
-	addr = flag.String("address", ":8080", "The address and port on which to serve HTTP.")
+	addr      = flag.String("address", ":8080", "The address and port on which to serve HTTP.")
+	appID     = flag.String("app", "", "Ecobee Registered App ID")
+	storePath = flag.String("store", "/tmp/promobee", "Persistent egobee credential store path")
 
-	// These flags should be set with the initial access/refresh tokens. The
-	// egobee library will refresh the tokens in memory during the life of the
-	// process.
-	// TODO(cfunkhouser): Use the persistent store when available.
-	accessToken  = flag.String("access_token", "", "Initial Ecobee API Access Token")
-	refreshToken = flag.String("refresh_token", "", "Initial Ecobee API Refresh Token")
-
-	tempMetric = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "temperature_fahrenheit",
-			Help: "Temperature in Fahrenheit as reported by an Ecobee sensor.",
-		},
-		[]string{"location"})
-
-	thermostatSelection = &egobee.Selection{
-		SelectionType:   egobee.SelectionTypeRegistered,
-		IncludeSettings: true,
-		IncludeRuntime:  true,
-		IncludeSensors:  true,
-	}
+	// The Ecobee API docs recommend polling no more frequently than 3 minutes.
+	pollInterval = time.Minute * 3
 )
 
 func init() {
-	prometheus.MustRegister(tempMetric)
+	flag.DurationVar(&pollInterval, "poll_interval", time.Minute*3, "Interval at which to poll the Ecobee API for updates.")
 }
 
 func main() {
-	log.Print("Starting...")
 	flag.Parse()
 
-	if *accessToken == "" || *refreshToken == "" {
-		log.Fatalf("--access_token and --refresh_token are both required.")
+	if *appID == "" {
+		log.Fatalf("--app is required")
+	}
+	if *storePath == "" {
+		log.Fatalf("--store is required")
 	}
 
+	ts, err := egobee.NewPersistentTokenFromDisk(*storePath)
+	if err != nil {
+		log.Fatalf("Failed to initialize store %q: %v", *storePath, err)
+	}
+
+	client := egobee.New(*appID, ts)
+	p := promobee.New(client, nil)
+
+	// Export the default metrics.
 	http.Handle("/metrics", promhttp.Handler())
 
-	ts := egobee.NewMemoryTokenStore(&egobee.TokenRefreshResponse{
-		AccessToken:  *accessToken,
-		RefreshToken: *refreshToken,
-		ExpiresIn:    egobee.TokenDuration{Duration: time.Minute * 15},
-	})
+	// Export Ecobee metrics
+	http.HandleFunc("/thermostats", p.ServeThermostatsList)
+	http.HandleFunc("/thermostat", p.ServeThermostat)
 
-	client := egobee.New(appID, ts)
-
-	populate := func() {
-		thermostats, err := client.Thermostats(thermostatSelection)
-		if err != nil {
-			log.Printf("Hrm, no good; skipping round: %+v", err)
-		}
-		for _, thermostat := range thermostats {
-			// TODO(cfunkhouser): Expose averages.
-			if len(thermostat.RemoteSensors) > 0 {
-				for _, sensor := range thermostat.RemoteSensors {
-					t, err := sensor.Temperature()
-					if err != nil {
-						log.Printf("Error getting temperature from %q: %v", sensor.Name, err)
-						continue
-					}
-					tempMetric.With(prometheus.Labels{"location": sensor.Name}).Set(t)
-				}
-			}
-		}
-	}
-
-	go func() {
-		// The Ecobee API docs recommend polling no more frequently than 3 minutes.
-		ticker := time.NewTicker(time.Minute * 3)
-		populate()
-		for {
-			select {
-			case <-ticker.C:
-				populate()
-			}
-		}
-
-	}()
-
+	log.Printf("Starting on %v", *addr)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
