0cccf4ec1963c9dff6640e766e583c3f981cd947
Christian Funkhouser <christian.funkhouser@gmail.com>
2019-03-09 17:13:27 -0500

    Fix a nil-pointer issue. Test the fix.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/cfunkhouser/egobee"
@@ -15,6 +16,7 @@ var (
 	addr      = flag.String("address", ":8080", "The address and port on which to serve HTTP.")
 	appID     = flag.String("app", "", "Ecobee Registered App ID")
 	storePath = flag.String("store", "/tmp/promobee", "Persistent egobee credential store path")
+	httpLog   = flag.String("http_log", "", "If set to a file path, all HTTP requests and responses will be logged there.")
 
 	// The Ecobee API docs recommend polling no more frequently than 3 minutes.
 	pollInterval = time.Minute * 3
@@ -34,12 +36,22 @@ func main() {
 		log.Fatalf("--store is required")
 	}
 
+	opts := &egobee.Options{}
+	if *httpLog != "" {
+		f, err := os.OpenFile(*httpLog, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatalf("Failed to open log file %q for append: %v", *httpLog, err)
+		}
+		opts.Log = true
+		opts.LogTo = f
+	}
+
 	ts, err := egobee.NewPersistentTokenFromDisk(*storePath)
 	if err != nil {
 		log.Fatalf("Failed to initialize store %q: %v", *storePath, err)
 	}
 
-	client := egobee.New(*appID, ts)
+	client := egobee.New(*appID, ts, opts)
 	p := promobee.New(client, nil)
 
 	// Export the default metrics.
